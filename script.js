10bb8d7bd9fc4c783dda3364d3315f1115b8da72
vaibhavyxx <vd5443@rit.edu>
2024-01-29 23:16:03 -0500

    tried +- effect

---
+++ script.js
@@ -0,0 +1,7 @@
+let web= document.getElementById('web');
+let games= document.getElementById('games');
+let xr= document.getElementById('xr');
+
+web.onclick= function(){
+    web.style.cursor = "grab";
+}
