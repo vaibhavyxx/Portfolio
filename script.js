cc4e5658f6e37574ebc339ea51e866193e42a6b7
vaibhavyxx <vd5443@rit.edu>
2024-04-24 21:32:56 -0400

    a bug with the collapsible menu

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-let doc = document;
-console.log(doc.innerHTML);
+function closeAll(){
+    let col = document.getElementsByClassName("collapsible");
+    for(let i=0; i < column.length; i++){
+        column[i].addEventListener("click", function(){
+            this.classList.toggle("active");
+            let content = this.nextElementSibling;
+                content.style.display = "none";
+        });}}
+
+let column = document.getElementsByClassName("collapsible");
+for(let i=0; i < column.length; i++){
+    column[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if(content.style.display === "block"){
+            content.style.display = "none";
+        }else{
+            content.style.display = "block";
+        }
+    });
+}
