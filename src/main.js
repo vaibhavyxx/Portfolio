af9f21f77fd3c5ef10adb25f1bf580e73537cb0f
vaibhavyxx <vd5443@rit.edu>
2025-02-07 18:12:50 -0500

    fixed the css and the position of navbar to be right below the header

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 const header = document.querySelector('header');
+const intro = document.querySelector('#intro');
 const nav = document.querySelector('#nav');
 let navVisible = false;
+const headerHeight = header.getBoundingClientRect().height;
 
+//displays navigation bar and positions it to be below the header 
+//at all times and is fixed 
 header.addEventListener('click', ()=> {
     if(!navVisible){
+        nav.style.position = "fixed";
+        nav.style.top = headerHeight+ 'px';
         nav.style.display = 'block';
         navVisible = true;
     }
