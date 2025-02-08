474161211dc795845cd19fdf25cc5b6fe0f4b5ab
vaibhavyxx <vd5443@rit.edu>
2025-02-08 15:14:11 -0500

    setup html, css and script in blog pages

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 const header = document.querySelector('header');
 const intro = document.querySelector('#intro');
 const nav = document.querySelector('#nav');
+const article = document.querySelector('#story');
 let navVisible = false;
 const headerHeight = header.getBoundingClientRect().height;
 
 //displays navigation bar and positions it to be below the header 
 //at all times and is fixed 
+window.onload= (event) =>{
+    console.log(headerHeight);
+    article.style.position= 'relative';
+    article.style.top = headerHeight +'px';
+}
+
 header.addEventListener('click', ()=> {
     if(!navVisible){
         nav.style.position = "fixed";
